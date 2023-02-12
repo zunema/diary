711abe659c654de5847ce574c8cde6f1bdcccf7d
zunema <yxxa.abc@gmail.com>
2023-02-12 17:45:16 +0900

    diaryエンティティ追加

--- src/main/java/com/yuya/diary/entity/Diary.java
+++ src/main/java/com/yuya/diary/entity/Diary.java
@@ -22,18 +22,63 @@ public class Diary {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
-    private String bodytext;
+    /* 出来事 */
+    @Column(name = "event")
+    private String event;
 
-    // 投稿日時
+    /* 投稿日時 */
     @Column(name = "create_datetime", nullable = false)
     private LocalDateTime createDatetime;
 
-    @Column
+    /* 気分 */
+    @Column(name = "feeling")
     private String feeling;
 
-    @Column
+    /* 感情 */
+    @Column(name = "emotions")
     private String emotions;
 
-    
+    /* やったこと */
+    @Column(name = "deed")
+    private String deed;
+
+    /* 明晰夢フラグ */
+    @Column(name = "lucid_dream_flg")
+    private Boolean lucidDream;
+
+    /* 起床 */
+    @Column(name = "awake")
+    private String awake;
+
+    /* ベッドにいる */
+    @Column(name = "inBed")
+    private String inBed;
+
+    /* 不明な睡眠 */
+    @Column(name = "asleep_unspesified")
+    private String asleepUnspesified;
+
+    /* 浅い睡眠 */
+    @Column(name = "asleep_core")
+    private String asleepCore;
+
+    /* 熟睡中 */
+    @Column(name = "asleep_deep")
+    private String asleepDeep;
+
+    /* レム睡眠 */
+    @Column(name = "asleep_rem")
+    private String asleepRem;
+
+    /* 睡眠時間 */
+    @Column(name = "asleep_time")
+    private String asleepTime;
+
+    /* 気づいたこと */
+    @Column(name = "noticed")
+    private String noticed;
+
+    /* 食べたもの */
+    @Column(name = "ate_today")
+    private String ateToday;
 }
