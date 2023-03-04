184221dc4b2536ae2631655b165cc281105a5b50
zunema <yxxa.abc@gmail.com>
2023-03-04 16:19:24 +0900

    削除機能作成

--- src/main/java/com/yuya/diary/controller/diaryController.java
+++ src/main/java/com/yuya/diary/controller/diaryController.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.yuya.diary.entity.Post;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import com.yuya.diary.repository.PostRepository;
@@ -23,4 +29,12 @@ public class diaryController {
         model.addAttribute("diaryList", diaryList);
         return "diary/index";
     }
+
+    // 指定されたidの日記を削除する
+    @PostMapping("delete")
+    public String delete(@RequestParam Integer id) {
+        postRepository.deleteById(id);
+        return "redirect:/diary/index";
+    }
+
 }
